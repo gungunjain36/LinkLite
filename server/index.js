9280c941dcc888942cf45b1de0a94b26b7da0169
gungunjain36 <jgungun1723@gmail.com>
2024-11-04 12:11:37 +0530

    backend: server error set up

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { redis } from "./utils/redis-client.js";
+
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send("<h1>Hello World!</h1>")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`);
+})
