40c214634af199b9a8d8bb5b96a5204bfa8f5887
gungunjain36 <jgungun1723@gmail.com>
2024-11-11 17:11:15 +0530

    git test

--- frontend/src/app/components/Button.tsx
+++ frontend/src/app/components/Button.tsx
@@ -2,6 +2,6 @@
 
 export default function Button(props: any) {
     return (
-        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Generate</button>
+        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Generate URL</button>
     )
 }
